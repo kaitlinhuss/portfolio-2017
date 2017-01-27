3e1b15915b9a5f631d12da2ab987c4d00229768d
Kaitlin Kidd <kkiddography@gmail.com>
2017-01-27 10:12:58 -0500

    Updated Skills.

--- js/portfolio-chart.js
+++ js/portfolio-chart.js
@@ -11,404 +11,405 @@
  /* ------------------------------------------------------------------------- */
  /* ------- Documentation: http://www.chartjs.org/docs/#radar-chart --------- */
  /* ------------------------------------------------------------------------- */
- /* 
-  * [01.] Chart 1 - web development skills. 
-  * [02.] Chart 2 - programming languages skills. 
-  * [03.] Chart 3 - project management skills. 
-  */ 
+ /*
+  * [01.] Radar Chart 1 - Languages
+  * [02.] Radar Chart 2 - Frameworks
+  * [03.] Radar Chart 3 - Tools
+	* [034] Radar Chart 4 - Design
+  */
 
-// Radar Chart 1 options.
-var skillsetOneOptions = {
-				
-	//Boolean - If we show the scale above the chart data			
+// [01.] Options
+var setOneOptions = {
+
+	//Boolean - If we show the scale above the chart data
 	scaleOverlay : false,
-	
+
 	//Boolean - If we want to override with a hard coded scale
-	scaleOverride : false,
-	
+	scaleOverride : true,
+
 	//** Required if scaleOverride is true **
 	//Number - The number of steps in a hard coded scale
-	scaleSteps : null,
+	scaleSteps : 10,
 	//Number - The value jump in the hard coded scale
-	scaleStepWidth : null,
+	scaleStepWidth : 10,
 	//Number - The centre starting value
-	scaleStartValue : null,
-	
+	scaleStartValue : 0,
+
 	//Boolean - Whether to show lines for each scale point
 	scaleShowLine : true,
 
-	//String - Colour of the scale line	
+	//String - Colour of the scale line
 	scaleLineColor : "#999",
-	
-	//Number - Pixel width of the scale line	
+
+	//Number - Pixel width of the scale line
 	scaleLineWidth : 1,
 
-	//Boolean - Whether to show labels on the scale	
+	//Boolean - Whether to show labels on the scale
 	scaleShowLabels : false,
-	
+
 	//Interpolated JS string - can access value
 	scaleLabel : "<%=value%>",
-	
+
 	//String - Scale label font declaration for the scale label
-	scaleFontFamily : "'Varela Round'",
-	
-	//Number - Scale label font size in pixels	
+	scaleFontFamily : "'Arial'",
+
+	//Number - Scale label font size in pixels
 	scaleFontSize : 12,
-	
-	//String - Scale label font weight style	
+
+	//String - Scale label font weight style
 	scaleFontStyle : "normal",
-	
-	//String - Scale label font colour	
+
+	//String - Scale label font colour
 	scaleFontColor : "#373f51",
-	
+
 	//Boolean - Show a backdrop to the scale label
 	scaleShowLabelBackdrop : true,
-	
-	//String - The colour of the label backdrop	
+
+	//String - The colour of the label backdrop
 	scaleBackdropColor : "rgba(255,255,255,0.75)",
-	
+
 	//Number - The backdrop padding above & below the label in pixels
 	scaleBackdropPaddingY : 2,
-	
-	//Number - The backdrop padding to the side of the label in pixels	
+
+	//Number - The backdrop padding to the side of the label in pixels
 	scaleBackdropPaddingX : 2,
-	
+
 	//Boolean - Whether we show the angle lines out of the radar
 	angleShowLineOut : true,
-	
+
 	//String - Colour of the angle line
 	angleLineColor : "rgba(255,255,255,0.3)",
-	
+
 	//Number - Pixel width of the angle line
 	angleLineWidth : 1,
-	
+
 	//String - Point label font declaration
-	pointLabelFontFamily : "'Varela Round'",
-	
+	pointLabelFontFamily : "'Arial'",
+
 	//String - Point label font weight
 	pointLabelFontStyle : "normal",
-	
-	//Number - Point label font size in pixels	
+
+	//Number - Point label font size in pixels
 	pointLabelFontSize : 12,
-	
-	//String - Point label font colour	
+
+	//String - Point label font colour
 	pointLabelFontColor : "#373f51",
-	
+
 	//Boolean - Whether to show a dot for each point
 	pointDot : false,
-	
+
 	//Number - Radius of each point dot in pixels
 	pointDotRadius : 3,
-	
+
 	//Number - Pixel width of point dot stroke
 	pointDotStrokeWidth : 1,
-	
+
 	//Boolean - Whether to show a stroke for datasets
 	datasetStroke : true,
-	
+
 	//Number - Pixel width of dataset stroke
 	datasetStrokeWidth : 1,
-	
+
 	//Boolean - Whether to fill the dataset with a colour
 	datasetFill : true,
-	
+
 	//Boolean - Whether to animate the chart
 	animation : true,
 
 	//Number - Number of animation steps
 	animationSteps : 60,
-	
+
 	//String - Animation easing effect
 	animationEasing : "easeOutQuart",
 
 	//Function - Fires when the animation is complete
 	onAnimationComplete : null
+
 }
 
-// Radar Chart 2 options. 
-var skillsetTwoOptions = {
-				
-	//Boolean - If we show the scale above the chart data			
-	scaleOverlay : false,
-	
-	//Boolean - If we want to override with a hard coded scale
-	scaleOverride : false,
-	
-	//** Required if scaleOverride is true **
-	//Number - The number of steps in a hard coded scale
-	scaleSteps : null,
-	//Number - The value jump in the hard coded scale
-	scaleStepWidth : null,
-	//Number - The centre starting value
-	scaleStartValue : null,
-	
-	//Boolean - Whether to show lines for each scale point
-	scaleShowLine : true,
-
-	//String - Colour of the scale line	
-	scaleLineColor : "#999",
-	
-	//Number - Pixel width of the scale line	
-	scaleLineWidth : 1,
-
-	//Boolean - Whether to show labels on the scale	
-	scaleShowLabels : false,
-	
-	//Interpolated JS string - can access value
-	scaleLabel : "<%=value%>",
-	
-	//String - Scale label font declaration for the scale label
-	scaleFontFamily : "'Varela Round'",
-	
-	//Number - Scale label font size in pixels	
-	scaleFontSize : 12,
-	
-	//String - Scale label font weight style	
-	scaleFontStyle : "normal",
-	
-	//String - Scale label font colour	
-	scaleFontColor : "#373f51",
-	
-	//Boolean - Show a backdrop to the scale label
-	scaleShowLabelBackdrop : true,
-	
-	//String - The colour of the label backdrop	
-	scaleBackdropColor : "rgba(255,255,255,0.75)",
-	
-	//Number - The backdrop padding above & below the label in pixels
-	scaleBackdropPaddingY : 2,
-	
-	//Number - The backdrop padding to the side of the label in pixels	
-	scaleBackdropPaddingX : 2,
-	
-	//Boolean - Whether we show the angle lines out of the radar
-	angleShowLineOut : true,
-	
-	//String - Colour of the angle line
-	angleLineColor : "rgba(255,255,255,0.3)",
-	
-	//Number - Pixel width of the angle line
-	angleLineWidth : 1,			
-	
-	//String - Point label font declaration
-	pointLabelFontFamily : "'Varela Round'",
-	
-	//String - Point label font weight
-	pointLabelFontStyle : "normal",
-	
-	//Number - Point label font size in pixels	
-	pointLabelFontSize : 12,
-	
-	//String - Point label font colour	
-	pointLabelFontColor : "#373f51",
-	
-	//Boolean - Whether to show a dot for each point
-	pointDot : false,
-	
-	//Number - Radius of each point dot in pixels
-	pointDotRadius : 3,
-	
-	//Number - Pixel width of point dot stroke
-	pointDotStrokeWidth : 1,
-	
-	//Boolean - Whether to show a stroke for datasets
-	datasetStroke : true,
-	
-	//Number - Pixel width of dataset stroke
-	datasetStrokeWidth : 1,
-	
-	//Boolean - Whether to fill the dataset with a colour
-	datasetFill : true,
-	
-	//Boolean - Whether to animate the chart
-	animation : true,
-
-	//Number - Number of animation steps
-	animationSteps : 60,
-	
-	//String - Animation easing effect
-	animationEasing : "easeOutQuart",
-
-	//Function - Fires when the animation is complete
-	onAnimationComplete : null
-	
-}
-
-// Radar Chart 3 options.
-var skillsetThreeOptions = {
-				
-	//Boolean - If we show the scale above the chart data			
-	scaleOverlay : false,
-	
-	//Boolean - If we want to override with a hard coded scale
-	scaleOverride : false,
-	
-	//** Required if scaleOverride is true **
-	//Number - The number of steps in a hard coded scale
-	scaleSteps : null,
-	//Number - The value jump in the hard coded scale
-	scaleStepWidth : null,
-	//Number - The centre starting value
-	scaleStartValue : null,
-	
-	//Boolean - Whether to show lines for each scale point
-	scaleShowLine : true,
-
-	//String - Colour of the scale line	
-	scaleLineColor : "#999",
-	
-	//Number - Pixel width of the scale line	
-	scaleLineWidth : 1,
-
-	//Boolean - Whether to show labels on the scale	
-	scaleShowLabels : false,
-	
-	//Interpolated JS string - can access value
-	scaleLabel : "<%=value%>",
-	
-	//String - Scale label font declaration for the scale label
-	scaleFontFamily : "'Varela Round'",
-	
-	//Number - Scale label font size in pixels	
-	scaleFontSize : 12,
-	
-	//String - Scale label font weight style	
-	scaleFontStyle : "normal",
-	
-	//String - Scale label font colour	
-	scaleFontColor : "#373f51",
-	
-	//Boolean - Show a backdrop to the scale label
-	scaleShowLabelBackdrop : true,
-	
-	//String - The colour of the label backdrop	
-	scaleBackdropColor : "rgba(255,255,255,0.75)",
-	
-	//Number - The backdrop padding above & below the label in pixels
-	scaleBackdropPaddingY : 2,
-	
-	//Number - The backdrop padding to the side of the label in pixels	
-	scaleBackdropPaddingX : 2,
-	
-	//Boolean - Whether we show the angle lines out of the radar
-	angleShowLineOut : true,
-	
-	//String - Colour of the angle line
-	angleLineColor : "rgba(255,255,255,0.3)",
-	
-	//Number - Pixel width of the angle line
-	angleLineWidth : 1,			
-	
-	//String - Point label font declaration
-	pointLabelFontFamily : "'Varela Round'",
-	
-	//String - Point label font weight
-	pointLabelFontStyle : "normal",
-	
-	//Number - Point label font size in pixels	
-	pointLabelFontSize : 12,
-	
-	//String - Point label font colour	
-	pointLabelFontColor : "#373f51",
-	
-	//Boolean - Whether to show a dot for each point
-	pointDot : false,
-	
-	//Number - Radius of each point dot in pixels
-	pointDotRadius : 3,
-	
-	//Number - Pixel width of point dot stroke
-	pointDotStrokeWidth : 1,
-	
-	//Boolean - Whether to show a stroke for datasets
-	datasetStroke : true,
-	
-	//Number - Pixel width of dataset stroke
-	datasetStrokeWidth : 1,
-	
-	//Boolean - Whether to fill the dataset with a colour
-	datasetFill : true,
-	
-	//Boolean - Whether to animate the chart
-	animation : true,
-
-	//Number - Number of animation steps
-	animationSteps : 60,
-	
-	//String - Animation easing effect
-	animationEasing : "easeOutQuart",
-
-	//Function - Fires when the animation is complete
-	onAnimationComplete : null
-	
-}
-
-// Radar Chart 1 data. 
-var skillsetOneData = {
-	labels : ["HTML5","CSS3","Sass","Bootstrap","JavaScript","JQuery","PHP", "PSD", "Illustrator"],
+// [01.] Data
+var setOneData = {
+	labels : [
+		"HTML",
+		"CSS",
+		"JavaScript",
+		"JQuery",
+		"AJAX",
+		"PHP",
+		"SQL",
+		"Java"
+	],
 	datasets : [
 		{
 			fillColor : "#373f51",
 			strokeColor : "#373f51",
-			//data : [65,59,90,81,56,55,40]
-            data : [100,90,40,80,60,50,30,80,80]
-		},
-		{
-			fillColor : "rgba(151,187,205,0)",
-			strokeColor : "rgba(151,187,205,0)",
-			data : [28,48,40,19,96,27,100, 20, 20]
+			data : [90, 80, 65, 60, 50, 50, 40, 60]
 		}
 	]
 }
 
-// Radar Chart 2 data. 
-var skillsetTwoData = {
-	labels : ["Java","Spring","ASP.NET","C#","C++","SQL"],
+// [01.] Chart
+var ctx = document.getElementById("setOne").getContext("2d");
+var setOne = new Chart(ctx).Radar(setOneData, setOneOptions);
+
+// [02.] Options
+var setTwoOptions = {
+
+	//Boolean - If we show the scale above the chart data
+	scaleOverlay : false,
+
+	//Boolean - If we want to override with a hard coded scale
+	scaleOverride : true,
+
+	//** Required if scaleOverride is true **
+	//Number - The number of steps in a hard coded scale
+	scaleSteps : 10,
+	//Number - The value jump in the hard coded scale
+	scaleStepWidth : 10,
+	//Number - The centre starting value
+	scaleStartValue : 0,
+
+	//Boolean - Whether to show lines for each scale point
+	scaleShowLine : true,
+
+	//String - Colour of the scale line
+	scaleLineColor : "#999",
+
+	//Number - Pixel width of the scale line
+	scaleLineWidth : 1,
+
+	//Boolean - Whether to show labels on the scale
+	scaleShowLabels : false,
+
+	//Interpolated JS string - can access value
+	scaleLabel : "<%=value%>",
+
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily : "'Arial'",
+
+	//Number - Scale label font size in pixels
+	scaleFontSize : 12,
+
+	//String - Scale label font weight style
+	scaleFontStyle : "normal",
+
+	//String - Scale label font colour
+	scaleFontColor : "#58a4b0",
+
+	//Boolean - Show a backdrop to the scale label
+	scaleShowLabelBackdrop : true,
+
+	//String - The colour of the label backdrop
+	scaleBackdropColor : "rgba(255,255,255,0.75)",
+
+	//Number - The backdrop padding above & below the label in pixels
+	scaleBackdropPaddingY : 2,
+
+	//Number - The backdrop padding to the side of the label in pixels
+	scaleBackdropPaddingX : 2,
+
+	//Boolean - Whether we show the angle lines out of the radar
+	angleShowLineOut : true,
+
+	//String - Colour of the angle line
+	angleLineColor : "rgba(255,255,255,0.3)",
+
+	//Number - Pixel width of the angle line
+	angleLineWidth : 1,
+
+	//String - Point label font declaration
+	pointLabelFontFamily : "'Arial'",
+
+	//String - Point label font weight
+	pointLabelFontStyle : "normal",
+
+	//Number - Point label font size in pixels
+	pointLabelFontSize : 12,
+
+	//String - Point label font colour
+	pointLabelFontColor : "#58a4b0",
+
+	//Boolean - Whether to show a dot for each point
+	pointDot : false,
+
+	//Number - Radius of each point dot in pixels
+	pointDotRadius : 3,
+
+	//Number - Pixel width of point dot stroke
+	pointDotStrokeWidth : 1,
+
+	//Boolean - Whether to show a stroke for datasets
+	datasetStroke : true,
+
+	//Number - Pixel width of dataset stroke
+	datasetStrokeWidth : 1,
+
+	//Boolean - Whether to fill the dataset with a colour
+	datasetFill : true,
+
+	//Boolean - Whether to animate the chart
+	animation : true,
+
+	//Number - Number of animation steps
+	animationSteps : 60,
+
+	//String - Animation easing effect
+	animationEasing : "easeOutQuart",
+
+	//Function - Fires when the animation is complete
+	onAnimationComplete : null
+
+}
+
+// [02.] Data
+var setTwoData = {
+	labels : [
+		"Bootstrap",
+		"AngularJS",
+		"Spring",
+		"Photoshop",
+		"Illustrator"
+	],
 	datasets : [
 		{
 			fillColor : "#58a4b0",
 			strokeColor : "#58a4b0",
-			//data : [65,59,90,81,56,55,40]
-            data : [80,70,50,50,60,50]
-		},
-		{
-			fillColor : "rgba(151,187,205,0)",
-			strokeColor : "rgba(151,187,205,0)",
-			data : [28,48,40,19,96,27]
+			data : [80, 40, 40, 70, 60]
 		}
 	]
 }
 
-// Radar Chart 3 data. 
-var skillsetThreeData = {
-	labels : ["Git","JIRA","Confluence","SourceTree", "Slack", "Asana"],
+// [02.] Chart
+var ctx = document.getElementById("setTwo").getContext("2d");
+var setTwo = new Chart(ctx).Radar(setTwoData, setTwoOptions);
+
+// [03.] Options
+var setThreeOptions = {
+
+	//Boolean - If we show the scale above the chart data
+	scaleOverlay : false,
+
+	//Boolean - If we want to override with a hard coded scale
+	scaleOverride : true,
+
+	//** Required if scaleOverride is true **
+	//Number - The number of steps in a hard coded scale
+	scaleSteps : 10,
+	//Number - The value jump in the hard coded scale
+	scaleStepWidth : 10,
+	//Number - The centre starting value
+	scaleStartValue : 0,
+
+	//Boolean - Whether to show lines for each scale point
+	scaleShowLine : true,
+
+	//String - Colour of the scale line
+	scaleLineColor : "#999",
+
+	//Number - Pixel width of the scale line
+	scaleLineWidth : 1,
+
+	//Boolean - Whether to show labels on the scale
+	scaleShowLabels : false,
+
+	//Interpolated JS string - can access value
+	scaleLabel : "<%=value%>",
+
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily : "'Arial'",
+
+	//Number - Scale label font size in pixels
+	scaleFontSize : 12,
+
+	//String - Scale label font weight style
+	scaleFontStyle : "normal",
+
+	//String - Scale label font colour
+	scaleFontColor : "#daa49a",
+
+	//Boolean - Show a backdrop to the scale label
+	scaleShowLabelBackdrop : true,
+
+	//String - The colour of the label backdrop
+	scaleBackdropColor : "rgba(255,255,255,0.75)",
+
+	//Number - The backdrop padding above & below the label in pixels
+	scaleBackdropPaddingY : 2,
+
+	//Number - The backdrop padding to the side of the label in pixels
+	scaleBackdropPaddingX : 2,
+
+	//Boolean - Whether we show the angle lines out of the radar
+	angleShowLineOut : true,
+
+	//String - Colour of the angle line
+	angleLineColor : "rgba(255,255,255,0.3)",
+
+	//Number - Pixel width of the angle line
+	angleLineWidth : 1,
+
+	//String - Point label font declaration
+	pointLabelFontFamily : "'Arial'",
+
+	//String - Point label font weight
+	pointLabelFontStyle : "normal",
+
+	//Number - Point label font size in pixels
+	pointLabelFontSize : 12,
+
+	//String - Point label font colour
+	pointLabelFontColor : "#daa49a",
+
+	//Boolean - Whether to show a dot for each point
+	pointDot : false,
+
+	//Number - Radius of each point dot in pixels
+	pointDotRadius : 3,
+
+	//Number - Pixel width of point dot stroke
+	pointDotStrokeWidth : 1,
+
+	//Boolean - Whether to show a stroke for datasets
+	datasetStroke : true,
+
+	//Number - Pixel width of dataset stroke
+	datasetStrokeWidth : 1,
+
+	//Boolean - Whether to fill the dataset with a colour
+	datasetFill : true,
+
+	//Boolean - Whether to animate the chart
+	animation : true,
+
+	//Number - Number of animation steps
+	animationSteps : 60,
+
+	//String - Animation easing effect
+	animationEasing : "easeOutQuart",
+
+	//Function - Fires when the animation is complete
+	onAnimationComplete : null
+
+}
+
+// [03.] Data
+var setThreeData = {
+	labels : [
+		"Git",
+		"SourceTree",
+		"Slack",
+		"Confluence",
+		"MySQL"
+	],
 	datasets : [
 		{
 			fillColor : "#daa49a",
 			strokeColor : "#daa49a",
-			//data : [65,59,90,81,56,55,40]
-            data : [80,60,60,70,70,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0)",
-			strokeColor : "rgba(151,187,205,0)",
-			data : [28,48,40,19,96,27,100]
+			data : [60, 70, 50, 50, 40]
 		}
 	]
 }
 
-// [01.] Radar Chart 1
-
-// Get the context of the Radar Chart canvas element we want to select
-var ctx = document.getElementById("setOne").getContext("2d");
-
-// Create the Radar Chart
-var skillsetOne = new Chart(ctx).Radar(skillsetOneData, skillsetOneOptions);
-
-// [02.] Radar Chart 2
-var ctx = document.getElementById("setTwo").getContext("2d");
-var skillsetTwo = new Chart(ctx).Radar(skillsetTwoData, skillsetTwoOptions);
-
-// [03.] Radar Chart 3
+// [03.] Chart
 var ctx = document.getElementById("setThree").getContext("2d");
-var skillsetThree = new Chart(ctx).Radar(skillsetThreeData, skillsetThreeOptions);
+var setThree = new Chart(ctx).Radar(setThreeData, setThreeOptions);
